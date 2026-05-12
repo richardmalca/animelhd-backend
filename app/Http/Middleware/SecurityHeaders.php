a0ca60a06306c5a3f560d23037953202af587bf2
Richard Malca <rmalcac16_1@unc.edu.pe>
2026-05-12 15:13:32 -0500

    feat: add SecurityHeaders middleware to configure standard HTTP security headers

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -15,32 +15,8 @@ class SecurityHeaders
         $response->headers->set('X-Content-Type-Options', 'nosniff');
 
         if ($request->is('v/*')) {
-            $envDomains = env('ALLOWED_IFRAME_DOMAINS', '');
-            $allowedDomains = !empty($envDomains) ? explode(',', $envDomains) : [];
-            $allowedDomains = array_map('trim', $allowedDomains);
-
-            $rawReferer = $request->headers->get('referer') ?? $request->headers->get('origin');
-            $requestHost = parse_url($rawReferer, PHP_URL_HOST);
-
-            $valid = false;
-
-            if ($requestHost) {
-                foreach ($allowedDomains as $domain) {
-                    $domainHost = parse_url($domain, PHP_URL_HOST) ?? $domain;
-                    if ($requestHost === $domainHost) {
-                        $valid = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!$valid) {
-                abort(403);
-            }
-
-            $response->headers->set('Content-Security-Policy', "frame-ancestors " . implode(' ', $allowedDomains));
+            $response->headers->set('Content-Security-Policy', "frame-ancestors *");
             $response->headers->remove('X-Frame-Options');
-
         } else {
             $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
         }
