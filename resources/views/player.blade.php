4b06332d6436d939f5d48de3a531a6095c43e530
Richard Malca Chuquimango <82131700+richardmalca@users.noreply.github.com>
2026-07-15 12:21:04 -0500

    Improve CSS formatting and comment out script
    Refactor CSS styles for better readability and comment out script tag.

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -5,13 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reproductor</title>
     <style>
-        body, html { margin: 0; padding: 0; height: 100%; width: 100%; overflow: hidden; background-color: #000; }
-        iframe { border: none; height: 100%; width: 100%; }
+        body, html {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+            overflow: hidden;
+            background-color: #000;
+        }
+
+        iframe {
+            border: none;
+            height: 100%;
+            width: 100%;
+        }
     </style>
 </head>
 <body>
     <iframe src="{{ $url }}" allowfullscreen scrolling="no" allow="autoplay; fullscreen"></iframe>
 
-    <script>(function(s){s.dataset.zone='11039557',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
+    {{--
+    <script>
+        (function(s){
+            s.dataset.zone='11039557',
+            s.src='https://al5sm.com/tag.min.js'
+        })([document.documentElement, document.body]
+            .filter(Boolean)
+            .pop()
+            .appendChild(document.createElement('script')))
+    </script>
+    --}}
 </body>
 </html>
