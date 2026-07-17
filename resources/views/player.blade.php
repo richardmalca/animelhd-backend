c0a19c18cd0086165d66a551c64ffe018a0b6981
Richard Malca <rmalcac16_1@unc.edu.pe>
2026-07-17 15:59:12 -0500

    restore ad script in backend player

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -23,5 +23,12 @@
 </head>
 <body>
     <iframe src="{{ $url }}" allowfullscreen scrolling="no" allow="autoplay; fullscreen"></iframe>
+
+    <script>
+        (function(s){
+            s.dataset.zone='11307964';
+            s.src='https://llvpn.com/tag.min.js';
+        })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
+    </script>
 </body>
 </html>
